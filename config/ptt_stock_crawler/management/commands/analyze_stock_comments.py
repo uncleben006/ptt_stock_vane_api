9f59257f287f7f8631e1e5bd6b0e5ca8806cf9ba
benwangnextgen <benwang@nextgen.com.hk>
2024-10-12 00:25:35 +0800

    Update analyze_stock_comments.py desc

--- config/ptt_stock_crawler/management/commands/analyze_stock_comments.py
+++ config/ptt_stock_crawler/management/commands/analyze_stock_comments.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from ptt_stock_crawler.services.analyze_stock_comments import analyze_stock_comments
 
 class Command(BaseCommand):
-    help = 'Call GPT API to analyze every comment\'s stock sentiment and stock target.'
+    help = 'Segment the comments and use Levenshtein distance to search for words similar to those in the stock dictionary. Finally, let GPT determine whether the comment mentions a company.'
 
     def handle(self, *args, **kwargs):
         analyze_stock_comments()
